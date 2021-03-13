b272b148ebd082db7badd879f37c9e75696b3f09
jmdesrosi <36333469+jmdesrosi@users.noreply.github.com>
2021-03-13 18:32:59 +0000

    Changed from Curseforge to Forge

--- startup.sh
+++ startup.sh
@@ -1,4 +1,1 @@
-cd /src/minecraft/world/
-#wget https://media.forgecdn.net/files/2690/320/FTB+Presents+Direwolf20+1.12-1.12.2-2.5.0-Server.zip
-
-docker run -d -p 25565:25565 -e EULA=TRUE -e INIT_MEMORY=8g -e MAX_MEMORY=14g -e USE_AIKAR_FLAGS=true -e VIEW_DISTANCE=6 -e GUI=FALSE -e TYPE=CURSEFORGE -e CF_SERVER_MOD=FTB+Presents+Direwolf20+1.12-1.12.2-2.5.0-Server.zip -v /src/minecraft/world:/data --name mc itzg/minecraft-server:java8-multiarch
+docker run -d -p 25565:25565 -e EULA=TRUE -e INIT_MEMORY=8g -e MAX_MEMORY=14g -e USE_AIKAR_FLAGS=true -e VIEW_DISTANCE=6 -e GUI=FALSE -e TYPE=FORGE -e VERSION=1.12.2 -v /src/minecraft/world:/data --name mc itzg/minecraft-server:java8-multiarch
